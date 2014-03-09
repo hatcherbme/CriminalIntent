70b2207692a2fe6586b53fd4bdd4d584bd0617ad
hatcherbme <hatcherbme@aol.com>
2014-03-09 01:17:57 -0500

    Chap 16 Action Bar add

--- CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -12,18 +12,16 @@ public class CrimeLab {
 	private CrimeLab(Context appContext) {
 		mAppContext = appContext;
 		mCrimes = new ArrayList<Crime>();
-		for (int i = 0; i < 100; i++) {
-			Crime c = new Crime();
-			c.setTitle("Crime #" + i);
-			c.setSolved(i % 2 == 0); // Every other one
-			mCrimes.add(c);
-		}
 	}
 	public static CrimeLab get(Context c) {
 		if (sCrimeLab == null) {
 			sCrimeLab = new CrimeLab(c.getApplicationContext());
 		}
 	return sCrimeLab;
+	}
+	
+	public void addCrime(Crime c) {
+		mCrimes.add(c);
 	}
 	
 	public ArrayList<Crime> getCrimes() {
